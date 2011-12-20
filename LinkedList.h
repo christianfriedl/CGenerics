d1d07f81690fa84291e4c7ffe6698c8d19edfb9d
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2011-12-20 17:14:19 +0100

    fix bugs

--- LinkedList.h
+++ LinkedList.h
@@ -29,7 +29,7 @@ typedef struct { \
 } LinkedListOf##TYPENAME; \
 \
 LinkedListElementOf##TYPENAME* LinkedListElementOf##TYPENAME##__new(AppState* appState, TYPENAME* value) { \
-    ArrayOf##TYPENAME* this = malloc(sizeof(*this)); \
+    LinkedListElementOf##TYPENAME* this = malloc(sizeof(*this)); \
     if (this != NULL) { \
         this->value = value; \
     } else \
@@ -37,8 +37,7 @@ LinkedListElementOf##TYPENAME* LinkedListElementOf##TYPENAME##__new(AppState* ap
     return this; \
 } \
 \
-void LinkedListElementOf##TYPENAME_delete(AppState* appState, LinkedListElementOf##TYPENAME* this) { \
-	TYPENAME_delete(this->value); \
+void LinkedListElementOf##TYPENAME##_delete(AppState* appState, LinkedListElementOf##TYPENAME* this) { \
 	free(this); \
 } \
 \
@@ -75,28 +74,20 @@ void LinkedListOf##TYPENAME##_insertElementAfter(AppState* appState, LinkedListO
 	} \
 } \
 \
-LinkedListElementOf##TYPENAME* LinkedListOf##TYPENAME##_remove(AppState* appState, LinkedListOf##TYPENAME* this, LinkedListElementOf##TYPENAME* element) { \
+LinkedListElementOf##TYPENAME* LinkedListOf##TYPENAME##_remove(AppState* appState, LinkedListOf##TYPENAME* this, LinkedListElementOf##TYPENAME* element) { }\
 	
 	
 
 /* type definition */
 
-#define Array(TYPENAME) ArrayOf##TYPENAME
+#define LinkedListElement(TYPENAME) LinkedListElementOf##TYPENAME
+#define LinkedList(TYPENAME) LinkedListOf##TYPENAME
 
 
 /* callers */
 
-#define Array__new(TYPENAME, initialCapacity) ArrayOf##TYPENAME##__new((initialCapacity))
-#define Array_delete(TYPENAME, array) ArrayOf##TYPENAME##_delete((array))
-#define Array_add(TYPENAME, array, value) ArrayOf##TYPENAME##_add((array), (value))
-#define Array_getValueAt(TYPENAME, array, at) ArrayOf##TYPENAME##_getValueAt((array), (at))
-#define Array_push Array_add
-#define Array_pop(TYPENAME, array) ArrayOf##TYPENAME##_pop((array))
-#define Array_getCapacity(TYPENAME, array) ArrayOf##TYPENAME##_getCapacity((array))
-#define Array_getSize(TYPENAME, array) ArrayOf##TYPENAME##_getSize((array))
-#define Array_shift(TYPENAME, array) ArrayOf##TYPENAME##_shift((array))
-#define Array_unshift(TYPENAME, array, value) ArrayOf##TYPENAME##_unshift((array), (value))
-#define Array_insertValueAt(TYPENAME, array, value, at) ArrayOf##TYPENAME##_insertValueAt((array), (value), (at))
-#define Array_removeValueAt(TYPENAME, array, at) ArrayOf##TYPENAME##_removeValueAt((array), (at))
+#define LinkedListElement__new(appState, TYPENAME, value) LinkedListElementOf##TYPENAME##__new((appState), (value))
+#define LinkedListElement_delete(appState, TYPENAME, element) LinkedListElementOf##TYPENAME##_delete((appState), element)
 
 
+#endif
