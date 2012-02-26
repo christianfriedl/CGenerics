ce983e50f3b979ae42a5e7d4093164a715b0596d
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2012-02-26 08:31:39 +0100

    better indentation

--- src/CGArrayIterator.h
+++ src/CGArrayIterator.h
@@ -13,69 +13,71 @@ typedef struct { \
 \
 
 #define DECLARE_ARRAY_ITERATOR_FUNCS(TYPENAME) \
-CGArrayOf##TYPENAME##Iterator* CGArrayOf##TYPENAME##Iterator__new(CGArrayOf##TYPENAME* array); \
-bool CGArrayOf##TYPENAME##Iterator_isInsideBounds(CGArrayOf##TYPENAME##Iterator* this); \
-void CGArrayOf##TYPENAME##Iterator_delete(CGArrayOf##TYPENAME##Iterator* this); \
-unsigned int CGArrayOf##TYPENAME##Iterator_getCurrentIndex(CGArrayOf##TYPENAME##Iterator* this); \
-TYPENAME* CGArrayOf##TYPENAME##Iterator_getCurrentElement(CGArrayOf##TYPENAME##Iterator* this); \
-bool CGArrayOf##TYPENAME##Iterator_moveToNextElement(CGArrayOf##TYPENAME##Iterator* this); \
-TYPENAME* CGArrayOf##TYPENAME##Iterator_getNextElement(CGArrayOf##TYPENAME##Iterator* this); \
-void CGArrayOf##TYPENAME##Iterator_reset(CGArrayOf##TYPENAME##Iterator* this); \
+\
+    CGArrayOf##TYPENAME##Iterator* CGArrayOf##TYPENAME##Iterator__new(CGArrayOf##TYPENAME* array); \
+    bool CGArrayOf##TYPENAME##Iterator_isInsideBounds(CGArrayOf##TYPENAME##Iterator* this); \
+    void CGArrayOf##TYPENAME##Iterator_delete(CGArrayOf##TYPENAME##Iterator* this); \
+    unsigned int CGArrayOf##TYPENAME##Iterator_getCurrentIndex(CGArrayOf##TYPENAME##Iterator* this); \
+    TYPENAME* CGArrayOf##TYPENAME##Iterator_getCurrentElement(CGArrayOf##TYPENAME##Iterator* this); \
+    bool CGArrayOf##TYPENAME##Iterator_moveToNextElement(CGArrayOf##TYPENAME##Iterator* this); \
+    TYPENAME* CGArrayOf##TYPENAME##Iterator_getNextElement(CGArrayOf##TYPENAME##Iterator* this); \
+    void CGArrayOf##TYPENAME##Iterator_reset(CGArrayOf##TYPENAME##Iterator* this); \
 \
 
 #define DEFINE_ARRAY_ITERATOR_FUNCS(TYPENAME) \
-CGArrayOf##TYPENAME##Iterator* CGArrayOf##TYPENAME##Iterator__new(CGArrayOf##TYPENAME* array) { \
-    CGArrayOf##TYPENAME##Iterator* this = malloc(sizeof(*this)); \
-    if (this != NULL) { \
-        this->array = array; \
-        this->currentIndex = 0; \
-    } else \
-        CGAppState_throwException(CGAppState__getInstance(), CGException__new(Severity_error, CGExceptionID_CannotAllocate, "cannot allocate CGArrayIterator")); \
-    return this; \
-} \
 \
-void CGArrayOf##TYPENAME##Iterator_delete(CGArrayOf##TYPENAME##Iterator* this) { \
-    free(this); \
-} \
-\
-/** \
-    NOTE: this works because the unsigned currentIndex will overflow and become INT_MAX if you subtract from it and it is 0 \
-*/ \
-bool CGArrayOf##TYPENAME##Iterator_isInsideBounds(CGArrayOf##TYPENAME##Iterator* this) { \
-    return (CGArrayOf##TYPENAME##_getSize(this->array) > this->currentIndex) ? true : false; \
-} \
-\
-unsigned int CGArrayOf##TYPENAME##Iterator_getCurrentIndex(CGArrayOf##TYPENAME##Iterator* this) { \
-    return this->currentIndex; \
-} \
-\
-TYPENAME* CGArrayOf##TYPENAME##Iterator_getCurrentElement(CGArrayOf##TYPENAME##Iterator* this) { \
-    if (CGArrayOf##TYPENAME##Iterator_isInsideBounds(this)) \
-        return CGArrayOf##TYPENAME##_getValueAt(this->array, this->currentIndex); \
-    else \
-        return NULL; \
-} \
-\
-bool CGArrayOf##TYPENAME##Iterator_moveToNextElement(CGArrayOf##TYPENAME##Iterator* this) { \
-    this->currentIndex++; \
-    if (CGArrayOf##TYPENAME##Iterator_isInsideBounds(this)) \
-        return true; \
-    else \
-        return false; \
-} \
-\
-TYPENAME* CGArrayOf##TYPENAME##Iterator_getNextElement(CGArrayOf##TYPENAME##Iterator* this) { \
-    if (CGArrayOf##TYPENAME##Iterator_moveToNextElement(this)) \
-        return CGArrayOf##TYPENAME##_getValueAt(this->array, this->currentIndex); \
-    else { \
-        CGAppState_throwException(CGAppState__getInstance(), CGException__new(Severity_error, CGExceptionID_ArrayIndexOutOfBounds, "Cannot allocate CGArrayIterator")); \
-        return NULL; \
+    CGArrayOf##TYPENAME##Iterator* CGArrayOf##TYPENAME##Iterator__new(CGArrayOf##TYPENAME* array) { \
+        CGArrayOf##TYPENAME##Iterator* this = malloc(sizeof(*this)); \
+        if (this != NULL) { \
+            this->array = array; \
+            this->currentIndex = 0; \
+        } else \
+            CGAppState_throwException(CGAppState__getInstance(), CGException__new(Severity_error, CGExceptionID_CannotAllocate, "cannot allocate CGArrayIterator")); \
+        return this; \
     } \
-} \
-\
-void CGArrayOf##TYPENAME##Iterator_reset(CGArrayOf##TYPENAME##Iterator* this) { \
-    this->currentIndex = 0; \
-} \
+    \
+    void CGArrayOf##TYPENAME##Iterator_delete(CGArrayOf##TYPENAME##Iterator* this) { \
+        free(this); \
+    } \
+    \
+    /** \
+        NOTE: this works because the unsigned currentIndex will overflow and become INT_MAX if you subtract from it and it is 0 \
+    */ \
+    bool CGArrayOf##TYPENAME##Iterator_isInsideBounds(CGArrayOf##TYPENAME##Iterator* this) { \
+        return (CGArrayOf##TYPENAME##_getSize(this->array) > this->currentIndex) ? true : false; \
+    } \
+    \
+    unsigned int CGArrayOf##TYPENAME##Iterator_getCurrentIndex(CGArrayOf##TYPENAME##Iterator* this) { \
+        return this->currentIndex; \
+    } \
+    \
+    TYPENAME* CGArrayOf##TYPENAME##Iterator_getCurrentElement(CGArrayOf##TYPENAME##Iterator* this) { \
+        if (CGArrayOf##TYPENAME##Iterator_isInsideBounds(this)) \
+            return CGArrayOf##TYPENAME##_getValueAt(this->array, this->currentIndex); \
+        else \
+            return NULL; \
+    } \
+    \
+    bool CGArrayOf##TYPENAME##Iterator_moveToNextElement(CGArrayOf##TYPENAME##Iterator* this) { \
+        this->currentIndex++; \
+        if (CGArrayOf##TYPENAME##Iterator_isInsideBounds(this)) \
+            return true; \
+        else \
+            return false; \
+    } \
+    \
+    TYPENAME* CGArrayOf##TYPENAME##Iterator_getNextElement(CGArrayOf##TYPENAME##Iterator* this) { \
+        if (CGArrayOf##TYPENAME##Iterator_moveToNextElement(this)) \
+            return CGArrayOf##TYPENAME##_getValueAt(this->array, this->currentIndex); \
+        else { \
+            CGAppState_throwException(CGAppState__getInstance(), CGException__new(Severity_error, CGExceptionID_ArrayIndexOutOfBounds, "Cannot allocate CGArrayIterator")); \
+            return NULL; \
+        } \
+    } \
+    \
+    void CGArrayOf##TYPENAME##Iterator_reset(CGArrayOf##TYPENAME##Iterator* this) { \
+        this->currentIndex = 0; \
+    } \
 \
 
 #define DECLARE_ARRAY_ITERATOR(TYPENAME) \
